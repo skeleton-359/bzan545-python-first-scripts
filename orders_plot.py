8d0f58696bccd353fb9a12b93561b2c047d40243
Ian Skelly <icatskell@icloud.com>
2026-07-14 10:42:35 -0400

    looked at this directory

--- orders_plot.py
+++ orders_plot.py
@@ -1,14 +1,18 @@
+# Importing necessary packages
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+# Creating dataset called orders
 orders = pd.read_csv("data/orders.csv")
 
+# Printing info for plot including the top rows and shape of df
 print("First rows")
 print(orders.head())
 
 print("Rows and columns")
 print(orders.shape)
 
+# Creating scatterplot
 sns.scatterplot(x="quantity", y="order_total", hue="category", data=orders)
 plt.show()
